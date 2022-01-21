0dbed80fb37170720dc34e50bffaa793540f4fab
Rki0 <86224851+Rki0@users.noreply.github.com>
2022-01-21 17:17:37 +0900

    add year, month, date
    month will be displayed like Jan, Feb, etc...

--- js/clock.js
+++ js/clock.js
@@ -10,3 +10,17 @@ function timeUpdate(){
 
 timeUpdate();
 setInterval(timeUpdate, 1000);
+
+const calendar = document.querySelector(".calendar");
+const monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+function dateUpdate(){
+  const date = new Date();
+  const year = date.getFullYear();
+  /**getmonth()는 1월이 0으로 표시되기 때문에 +1을 해주는게 맞지만, monthList에서는 1월(Jan)의 index가 0번이므로 +1을 안해도 된다. */
+  const month = monthList[date.getMonth()];
+  const day = String(date.getDate()).padStart(2, 0);
+  calendar.innerText = `${month}.${day}, ${year}`;
+}
+
+dateUpdate();
